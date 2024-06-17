6e36c97fb8b051c59b2e8e2e5f8fa21cf0c8311e
jeet12 <rajkumar.soulrider.jeet47@gmail.com>
2024-06-17 21:09:52 +0000

    add creds

--- CorpApp.cs
+++ CorpApp.cs
@@ -15,7 +15,8 @@ namespace CorpApp
             // Combine the directory and filename to get the full path
             string outputPath = Path.Combine(outputDirectory, outputFile);
             //Runas
-
+            // Username: Administrator
+            // Password: Asa31904#!
             try
             {
                 // Get the list of currently running processes
